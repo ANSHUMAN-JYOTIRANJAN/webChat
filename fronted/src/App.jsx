e69213b70a7339f4c3352745a4ef73c6d7da82e4
ANSHUMAN-JYOTIRANJAN <ansumanswain456@gmail.com>
2025-12-31 18:49:57 +0530

    Fix keystroke sound path

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -5,7 +5,7 @@ import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import { useAuthStore } from "./store/useAuthStore";
 import PageLoader from "./components/PageLoader";
-import {Toaster} from "react-hot-toast"
+import { Toaster } from "react-hot-toast";
 
 export default function App() {
   const { checkAuth, isCheckingAuth, authUser } = useAuthStore();
@@ -16,21 +16,47 @@ export default function App() {
 
   console.log({ authUser });
 
-  if(isCheckingAuth){
-    return <PageLoader/>
+  if (isCheckingAuth) {
+    return <PageLoader />;
   }
   return (
     <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
-      <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
-      <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
+      <div
+        className="pointer-events-none absolute inset-0 
+    bg-[linear-gradient(to_right,#ffffff0f_1px,transparent_1px),
+    linear-gradient(to_bottom,#ffffff0f_1px,transparent_1px)]
+    bg-[size:18px_18px]"
+      />
+
+  
+      <div
+        className="pointer-events-none absolute top-[-6rem] -left-16 size-[28rem]
+    bg-gradient-to-r from-pink-500/30 to-purple-500/20 
+    blur-[120px] rounded-full"
+      />
+
+ 
+      <div
+        className="pointer-events-none absolute bottom-[-6rem] -right-16 size-[28rem]
+    bg-gradient-to-l from-cyan-500/30 to-blue-500/20 
+    blur-[120px] rounded-full"
+      />
       {/* <button onClick={login} className="z-10"></button> */}
       <Routes>
-        <Route path="/" element={authUser ?<ChatPage />: <Navigate to={"/login"}/>} />
-        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to={"/"}/>} />
-        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to={"/"}/>} />
+        <Route
+          path="/"
+          element={authUser ? <ChatPage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+        />
       </Routes>
-      <Toaster/>
+      <Toaster />
     </div>
   );
 }
