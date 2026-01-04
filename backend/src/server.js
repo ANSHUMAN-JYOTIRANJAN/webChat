a5fb0067d4bdc56dece8899df429453785bebb00
ANSHUMAN-JYOTIRANJAN <ansumanswain456@gmail.com>
2026-01-04 18:33:45 +0530

    change server.js file

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,9 +15,24 @@ const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 5000;
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://web-app-t4aj.onrender.com",
+];
+
 app.use(express.json({limit:"15mb"}));
 app.use(express.urlencoded({extended: true, limit:"15mb"}))
-app.use(cors({origin:ENV.CLIENT_URL, credentials:true}));
+app.use(
+  cors({
+    origin: (origin, cb) => {
+      if (!origin) return cb(null, true);
+      if (allowedOrigins.includes(origin)) return cb(null, true);
+      cb(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  })
+);
+app.options("*", cors());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
