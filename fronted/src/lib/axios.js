176affa49ce37a94001e03d30b2a87d0b70ed5a3
ANSHUMAN-JYOTIRANJAN <ansumanswain456@gmail.com>
2026-01-04 18:07:02 +0530

    axios page fronted

--- fronted/src/lib/axios.js
+++ fronted/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "https://webapplication-en67.onrender.com/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "https://chat-communication.onrender.com/api",
   withCredentials: true,
 });
