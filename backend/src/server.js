ca055fecdf5ed6fa772da5d32ceed3a4bb27f54c
ANSHUMAN-JYOTIRANJAN <ansumanswain456@gmail.com>
2026-01-30 14:54:11 +0530

    changes the server.js part

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const PORT = ENV.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://web-app-t4aj.onrender.com",
+  "https://chating-36up.onrender.com",
 ];
 
 app.use(express.json({limit:"15mb"}));
