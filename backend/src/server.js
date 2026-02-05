34dc4a0983d4944abe0386bba832f263849702ca
ANSHUMAN-JYOTIRANJAN <ansumanswain456@gmail.com>
2026-02-06 00:16:14 +0530

    changes the server part

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,36 +7,41 @@ import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import {app,server} from "./lib/socket.js"
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 const __dirname = path.resolve();
-
 
 const PORT = ENV.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:5173",
   "https://videoconferen.onrender.com",
-  "https://webchatapp-wxcc.onrender.com"
+  "https://webchatapp-wxcc.onrender.com",
 ];
 
-app.use(express.json({limit:"15mb"}));
-app.use(express.urlencoded({extended: true, limit:"15mb"}))
+app.use(express.json({ limit: "15mb" }));
+app.use(express.urlencoded({ extended: true, limit: "15mb" }));
+
 app.use(
   cors({
-    origin: function (origin, callback) {
+    origin: (origin, callback) => {
       if (!origin) return callback(null, true);
+
+      if (origin.startsWith("http://localhost")) {
+        return callback(null, true);
+      }
+
       if (allowedOrigins.includes(origin)) {
         return callback(null, true);
       }
+
       callback(new Error("Not allowed by CORS"));
     },
     credentials: true,
-  })
+  }),
 );
-//  app.options("*", cors());
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
