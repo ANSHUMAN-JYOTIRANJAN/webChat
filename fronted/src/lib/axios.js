ed23f069cd1d189083fe3b24721e96e8d79bce58
ANSHUMAN-JYOTIRANJAN <ansumanswain456@gmail.com>
2026-02-05 23:49:27 +0530

    add render.yaml

--- fronted/src/lib/axios.js
+++ fronted/src/lib/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+console.log("Environment:", import.meta.env);//
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.NODE === "development" ? "http://localhost:5000/api" : "https://videoconferenc.onrender.com/api",
   withCredentials: true,
